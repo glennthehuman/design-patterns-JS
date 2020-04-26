f8691e84e8a51d7c9725676ec18aca2dc5e497e1
Glenn Guden <g.guden@createit.pl>
2020-04-26 17:02:53 +0800

    used correct description

--- test/command-test.js
+++ test/command-test.js
@@ -3,8 +3,7 @@ const expect = require('chai').expect;
 const [Cockpit, Turbine, OnCommand, OffCommand, SpeedUpCommand, SpeedDownCommand] = require('../src/behavioral/command/command.js');
 
 /**
- * As the name suggests, the chain of responsibility pattern creates a chain of receiver objects for a request. This pattern decouples sender and receiver of a request based on type of request. This pattern comes under behavioral patterns.
- * In this pattern, normally each receiver contains reference to another receiver. If one object cannot handle the request then it passes the same to the next receiver and so on.
+ * Command pattern is a data driven design pattern and falls under behavioral pattern category. A request is wrapped under an object as command and passed to invoker object. Invoker object looks for the appropriate object which can handle this command and passes the command to the corresponding object which executes the command.
  */
 describe('command tests', () => {
 
